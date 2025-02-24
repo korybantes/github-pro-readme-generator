d066c761532be5bfaf79d9b2338be871844fef94
MORY 808 <ertacspotify@gmail.com>
2025-02-24 16:36:19 +0100

    reactstrictmode

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-eslint: {
-  ignoreDuringBuilds: true,
-},
-
-swcMinify: false,
-
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: false,
+  reactStrictMode: true
 };
 
 
